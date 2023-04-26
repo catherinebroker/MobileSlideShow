0096bcc1f1c87faf908d7c25cafa2151dc0e1803
Catherine Masaoka <39732189+catherinebroker@users.noreply.github.com>
2023-04-26 11:29:21 -0400

    Cleaned up extra styles and files.

--- src/App.vue
+++ src/App.vue
@@ -11,31 +11,17 @@ import MobileSlideShow from './components/MobileSlideShow.vue'
   </main>
 </template>
 
-<style scoped>
-/* header {
-  line-height: 1.5;
+<style>
+@import url(https://fonts.googleapis.com/css?family=Josefin+Slab:100);
+
+html,
+body {
+  height: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import './assets/main.css'
-// import './assets/mobile-slideshow.css'
-
 createApp(App).mount('#app')
